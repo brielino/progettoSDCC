e420d64e66d895edc3a45c064a4ff6216d40dc41
brielino <61842589+brielino@users.noreply.github.com>
2020-10-14 19:32:42 +0200

    Add files via upload

--- GoogleL.py
+++ GoogleL.py
@@ -21,35 +21,34 @@ class IlMioThreadMaps(Thread):
 
     def run(self):
         while True:
-            id_ambulance = 0
-            km, id_ambulance = calculate_distance(patient)
+            km, id_hospital = calculate_distance(patient)
             if km is None:
                 print("ambulanze non disponibili")
                 time.sleep(5)
                 print("riprovo")
             else:
-                print(id_ambulance)
+                print(id_hospital)
                 tempo = calculate_time(km)
                 time.sleep(tempo)
-                release_ambulance(id_ambulance)
+                release_ambulance(id_hospital)
                 print('ambulanza è libera')
                 return
 
 
 def release_ambulance(id_ambulance):
-    for ambulance in ambulances:
-        if ambulance['Id'] == id_ambulance:
-            ambulance['In Use'] = 0
+    for current_hospital in hospital:
+        if current_hospital['Id'] == id_ambulance:
+            current_hospital['Number of Ambulances'] = current_hospital['Number of Ambulances'] + 1
 
 
 def take_coordination_patient():
     global data
-    global ambulances
+    global hospital
     with open('attributes.json') as patients:
         data = json.load(patients)
         patients.close()
-    with open('ambulanze.json') as ambulance:
-        ambulances = json.load(ambulance)
+    with open('hospital.json') as ambulance:
+        hospital = json.load(ambulance)
         ambulance.close()
 
 
@@ -58,30 +57,30 @@ def calculate_distance(current_patient):
     km = 0
 
     coord_patient = (current_patient['Latitud'], current_patient['Longitud'])
-    for current_ambulance in ambulances:
-        if current_ambulance['In Use'] == 0:
-            coord_ambulance = (current_ambulance['Latitud'], current_ambulance['Longitud'])
+    for current_Hospital in hospital:
+        if current_Hospital['Number of Ambulances'] != 0:
+            coord_ambulance = (current_Hospital['Latitud'], current_Hospital['Longitud'])
             current_km = geodesic(coord_patient, coord_ambulance).kilometers
             if first == 0:
                 first = 1
                 km = current_km
-                previus_ambulance = current_ambulance
-                id_ambulance = current_ambulance['Id']
+                previus_hospital = current_Hospital
+                id_hospital = current_Hospital['Id']
                 threadLock.acquire()
-                current_ambulance['In Use'] = 1
+                current_Hospital['Number of Ambulances'] = current_Hospital['Number of Ambulances'] - 1
                 threadLock.release()
             elif km > current_km:
                 km = current_km
-                id_ambulance = current_ambulance['Id']
+                id_hospital = current_Hospital['Id']
                 threadLock.acquire()
-                previus_ambulance['In Use'] = 0
-                previus_ambulance = current_ambulance
-                current_ambulance['In Use'] = 1
+                previus_hospital['Number of Ambulances'] = previus_hospital['Number of Ambulances'] + 1
+                previus_hospital = current_Hospital
+                current_Hospital['Number of Ambulances'] = current_Hospital['Number of Ambulances'] - 1
                 threadLock.release()
 
     if first == 0:
         return None, None
-    return km, id_ambulance
+    return km, id_hospital
 
 
 def calculate_time(km):
